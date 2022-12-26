a144456085a43657836236139109be9324081edc
Italo <italofabricio@outlook.pt>
2022-12-26 18:06:32 -0300

    Remove log

--- api/src/index.js
+++ api/src/index.js
@@ -2,5 +2,5 @@ import startServer from "./startServer";
 
 import typeDefs from './graphql/typedefs';
 import resolvers from './graphql/resolvers';
-console.log('teste')
+
 startServer({ typeDefs, resolvers })
